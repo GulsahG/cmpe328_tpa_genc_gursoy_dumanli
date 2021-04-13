6d6a2dcdf2365a6b5b20128699e8c8d278cd3f4e
Gülşah Genç <gulsahgenc.gg@gmail.com>
2021-04-14 01:09:45 +0300

    Streaming with OBS is implemented

--- client/src/components/streams/StreamShow.js
+++ client/src/components/streams/StreamShow.js
@@ -1,11 +1,43 @@
 import React from 'react';
+import flv from 'flv.js';
 import { connect } from 'react-redux';
 import { fetchStream } from '../../actions';
 import { Button, Heading, Box } from '@chakra-ui/react';
 
 class StreamShow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount() {
-    this.props.fetchStream(this.props.match.params.id);
+    const { id } = this.props.match.params;
+
+    this.props.fetchStream(id);
+    this.buildPlayer();
+  }
+
+  componentDidUpdate() {
+    this.buildPlayer();
+  }
+
+  componentWillUnmount() {
+    this.player.destroy();
+  }
+
+  buildPlayer() {
+    if(this.player || !this.props.stream) {
+      return;
+    }
+
+    const { id } = this.props.match.params;
+
+    this.player = flv.createPlayer({
+      type: "flv",
+      url: `http://localhost:8000/live/${id}.flv`,
+    });
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render(){
@@ -25,12 +57,21 @@ class StreamShow extends React.Component {
     return (
       <Box 
         m={{base: "5vw 0 0 15vw", md: "5vw 0 0 22.5vw"}} 
-        w={{base: "75vw", md:"60vw"}}
+        w={{base: "75vw", md: "60vw"}}
         p={2}
         d="flex"
         flexDir="column"
         alignItems="flex-start"
       >
+        <Box
+          w={{base: "65vw", md: "50vw"}}
+        >
+          <video 
+            ref={this.videoRef}
+            style={{ width: "100%", margin: "2.5vw 1.25vw" }}
+            controls
+          />
+        </Box>
         <Heading 
           m={{base: "3vw 0", md: "1.5vw 0"}}  
           as="h1" 
